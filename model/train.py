b92c129dab7c4a1dfd7de9d219ab183652fced29
Basudev-Das25 <basudevd983@gmail.com>
2025-12-29 13:42:24 +0530

    Trained Random Forest classifier and evaluated risk prediction

--- model/train.py
+++ model/train.py
@@ -1,5 +1,8 @@
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report, confusion_matrix
 
 #Load dataset
 data = pd.read_csv("../data/student_risk.csv")
@@ -8,16 +11,38 @@ data = pd.read_csv("../data/student_risk.csv")
 X = data.drop("risk_level", axis=1)
 Y = data["risk_level"]
 
-print("Feature sample:")
-print(Y.head())
+# print("Feature sample:")
+# print(Y.head())
 
 #Encode target labels
 label_encoder = LabelEncoder()
 Y_encoded = label_encoder.fit_transform(Y)
 
-print("\nTarget sample (after encoding):")
-print(Y_encoded[:5])
+#Train-test split
+X_train, X_test, Y_train, Y_test = train_test_split(
+    X,
+    Y_encoded,
+    test_size=0.4,
+    random_state=42,
+    stratify=Y_encoded
+)
 
-print("\nLabel mapping:")
-for i, label in enumerate(label_encoder.classes_):
-    print(f"{label} -> {i}")
+#train Random forest model
+model = RandomForestClassifier(
+    n_estimators=100,
+    max_depth=5,
+    random_state=42
+)
+
+model.fit(X_train, Y_train)
+
+
+#Predictions
+Y_pred = model.predict(X_test)
+
+#Evaluation
+print("Classification report:")
+print(classification_report(Y_test, Y_pred, target_names=label_encoder.classes_))
+
+print("\nConfussion Matrix")
+print(confusion_matrix(Y_test, Y_pred))
